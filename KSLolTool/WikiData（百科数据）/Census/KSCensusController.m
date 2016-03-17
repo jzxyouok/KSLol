7ca00ff48071aef58d6b377e36f682762d51865f
xiaoshi <sjl_leaf@163.com>
2016-03-17 15:19:44 +0800

    人口普查界面

--- KSLolTool/WikiData（百科数据）/Census/KSCensusController.m
+++ KSLolTool/WikiData（百科数据）/Census/KSCensusController.m
@@ -7,6 +7,7 @@
 //
 
 #import "KSCensusController.h"
+#import "KSFirstWebController.h"
 
 @interface KSCensusController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setWebViewWithWebType:WebTypeUrl urlString:@"http://h5v2.laoyuegou.com/lol/census.html" htmlString:nil title:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +26,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)openNewView:(NSString *)url
+{
+    KSFirstWebController *ctrl = [[KSFirstWebController alloc] init];
+    ctrl.urlStr = url;
+    [self.navigationController pushViewController:ctrl animated:YES];
 }
-*/
 
 @end
